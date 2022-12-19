8f546d6bf50daf494d14a34a0f3524135356a890
Túlio <tuliovictorp@hotmail.com>
2022-12-18 22:05:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function soma(a, b) {
+    return a + b;
+
+}
+
+function subtracao(a, b) {
+    return a - b;
+
+}
+
+function divisao(a, b) {
+    return a / b;
+
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+/*for (x = 3; x < 10; x = x + 2) {
+    console.log(x)
+}
+*/
+let x = 0
+var equacao = x == x+1;
+console.log(equacao)
